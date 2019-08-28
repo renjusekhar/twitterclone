a216de2cd08699aec12573c3b43117cd5c320410
renjusekhar <renjusekhar@outlook.com>
2019-08-28 10:50:24 +0530

    third

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Maincontainer from './container/Layout'
 
-const App =() =>{
+
+const App = () => {
   useEffect(() => {
     getUserData();
-  
+
   });
 
-  const getUserData = async() =>{
-    const response =  await  fetch(`https://drive.google.com/file/d/1Wdbh9eCHfPYaME884Sltagz6o0WvVUJU/view?usp=sharing`);
-    const data = await response.json();
-    console.log(data);
- }
+  // https://drive.google.com/file/d/1Wdbh9eCHfPYaME884Sltagz6o0WvVUJU/view?usp=sharing
+
+  const getUserData = async () => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://drive.google.com/file/d/1Wdbh9eCHfPYaME884Sltagz6o0WvVUJU/view?usp=sharing"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+      .then(response => response.text())
+      .then(contents => console.log(contents))
+      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+  }
   return (
     <div className="App">
-         <Maincontainer/>
+      <Maincontainer />
     </div>
   );
 }
 
 export default App;
+
